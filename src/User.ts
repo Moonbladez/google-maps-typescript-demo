fa3f215147e799fc024d1704a787439c447ae448
Bekki <moonbladez@hotmail.com>
2020-12-20 21:32:03 +0100

    company component added

--- src/User.ts
+++ src/User.ts
@@ -3,10 +3,23 @@ import faker from "faker";
 
 export class User {
   name: string;
+  surname: string;
+  email: string;
   location: {
     lat: number;
     lng: number;
   };
 
-  constructor() {}
+  constructor() {
+    this.name = faker.name.firstName();
+    this.surname = faker.name.lastName();
+    this.email = faker.internet
+      .email(this.name, this.surname)
+      .toLocaleLowerCase();
+    this.location = {
+      //change to number
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
 }
