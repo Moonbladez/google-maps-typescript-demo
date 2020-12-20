0b20ecbe78b267574d7f50aa747bfc9263beef50
Bekki <moonbladez@hotmail.com>
2020-12-20 21:44:34 +0100

    google maps install

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -100519,7 +100519,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38371" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45255" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
